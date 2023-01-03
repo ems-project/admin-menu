259d529fa0623d5a3b22fa7077614931c92d29ac
Mathieu De Keyzer <me@theus.be>
2023-01-03 10:30:28 +0100

    chore: standardjs

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 /**
  * Show admin edit button in FE
  */
-export default function showAdminMenu(targetAttr = '_blank', innerHTML = 'Back to ems', btnClass = 'btn btn-primary btn-sm emsch') {
-    const targets = document.querySelectorAll('[data-ems-key]');
-    [].forEach.call(targets, function(target, index) {
+export default function showAdminMenu (targetAttr = '_blank', innerHTML = 'Back to ems', btnClass = 'btn btn-primary btn-sm emsch') {
+  const targets = document.querySelectorAll('[data-ems-key]');
+  [].forEach.call(targets, function (target, index) {
+    const ouuid = target.getAttribute('data-ems-key')
+    const type = target.getAttribute('data-ems-type')
+    const url = target.getAttribute('data-ems-url')
 
-        const ouuid = target.getAttribute('data-ems-key');
-        const type = target.getAttribute('data-ems-type');
-        const url = target.getAttribute('data-ems-url');
+    const admBtn = document.createElement('a')
+    admBtn.innerHTML = innerHTML
+    admBtn.id = 'admin-menu-' + index
+    admBtn.setAttribute('class', btnClass)
+    admBtn.type = 'button'
+    admBtn.href = url + '/data/revisions/' + type + ':' + ouuid
+    if (typeof targetAttr === 'string' || targetAttr instanceof String) {
+      admBtn.target = targetAttr
+    }
 
-        const admBtn = document.createElement('a');
-        admBtn.innerHTML = innerHTML;
-        admBtn.id = 'admin-menu-' + index;
-        admBtn.setAttribute('class', btnClass);
-        admBtn.type = 'button';
-        admBtn.href  = url +'/data/revisions/'+ type +':'+ ouuid;
-        if (typeof targetAttr === 'string' || targetAttr instanceof String) {
-            admBtn.target  = targetAttr;
-        }
+    const admWrapper = document.createElement('div')
+    admWrapper.setAttribute('class', 'admin-menu-wrapper hide')
+    admWrapper.insertAdjacentElement('beforeend', admBtn)
 
-        const admWrapper = document.createElement('div');
-        admWrapper.setAttribute('class', 'admin-menu-wrapper hide');
-        admWrapper.insertAdjacentElement('beforeend', admBtn);
-
-        target.insertAdjacentElement('beforeend', admWrapper);
-        target.onmouseover = function() {
-            admWrapper.classList.remove("hide");
-        }
-        target.onmouseout = function() {
-            admWrapper.classList.add("hide");
-        }
-    });
+    target.insertAdjacentElement('beforeend', admWrapper)
+    target.onmouseover = function () {
+      admWrapper.classList.remove('hide')
+    }
+    target.onmouseout = function () {
+      admWrapper.classList.add('hide')
+    }
+  })
 }
